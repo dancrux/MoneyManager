e73cbcf34e2f4bc3a0c55e37553d9bcc260914a6
dancrux <nwogwugwudaniel@gmail.com>
2020-06-19 02:01:17 +0100

    Setup Nav Drawer, ui for home screen and theme

--- app/src/main/java/com/cruxrepublic/moneymanager/MainActivity.kt
+++ app/src/main/java/com/cruxrepublic/moneymanager/MainActivity.kt
@@ -1,32 +1,49 @@
 package com.cruxrepublic.moneymanager
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var toolbar: Toolbar
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        toolbar = findViewById(R.id.toolbar)
+
+       val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar,
+            0,0)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        val bottomNavView: BottomNavigationView = findViewById(R.id.bottom_nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(navView, navController)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.navigation_home, R.id.navigation_income, R.id.navigation_expense,
             R.id.navigation_received, R.id.navigation_sent))
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        bottomNavView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/cruxrepublic/moneymanager/ui/home/HomeFragment.kt
+++ app/src/main/java/com/cruxrepublic/moneymanager/ui/home/HomeFragment.kt
@@ -8,7 +8,6 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import com.cruxrepublic.moneymanager.R
 
 class HomeFragment : Fragment() {
@@ -23,7 +22,7 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
+        val textView: TextView = root.findViewById(R.id.totalFigureText)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
