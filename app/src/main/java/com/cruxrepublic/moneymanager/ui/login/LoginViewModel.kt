e56c1b0b227f021a81f72b3b9b4cbe1db1696e42
dancrux <nwogwugwudaniel@gmail.com>
2020-06-22 16:11:05 +0100

    User Registration (with Firebase auth) not working yet

---
+++ app/src/main/java/com/cruxrepublic/moneymanager/ui/login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.cruxrepublic.moneymanager.ui.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel: ViewModel() {
+
+}
---
+++ app/src/main/java/com/cruxrepublic/moneymanager/ui/signup/User.kt
@@ -0,0 +1,13 @@
+package com.cruxrepublic.moneymanager.ui.signup
+
+class User(
+    email: String,
+    firstName: String,
+    surname: String,
+    country: String,
+    age: String,
+    phoneNumber: String,
+    sex: String
+) {
+
+}
